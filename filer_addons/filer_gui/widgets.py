d4b6c979c19fcad48244955913d9204874cfcf61
alaric <alaric@wullerot.ch>
2017-06-20 18:17:53 +0200

    redo filer_gui widgets no longaer based on filer.widgets but now on rawidfield

--- filer_addons/filer_gui/widgets.py
+++ filer_addons/filer_gui/widgets.py
@@ -8,15 +8,17 @@ from django.template.loader import render_to_string
 from django.utils.http import urlencode
 from django.utils.safestring import mark_safe
 
-from filer.fields.file import AdminFileWidget
-
 
 logger = logging.getLogger(__name__)
 
 
-class FilerGuiFileWidget(AdminFileWidget):
+# TODO get rid of filer code and implement rawid field
+class FilerGuiFileWidget(ForeignKeyRawIdWidget):
+    """
+    Adwanced ForeignKeyRawIdWidget with preview and nice select
+    """
 
-    template_name = 'admin/filer_gui/widgets/admin_file_original.html'
+    template_name = 'admin/filer_gui/widgets/admin_file.html'
 
     class Media:
         css = {
@@ -24,12 +26,14 @@ class FilerGuiFileWidget(AdminFileWidget):
                 'admin/filer_gui/css/filer_gui.css',
             ]
         }
+        """
         js = (
             'filer/js/libs/dropzone.min.js',
             'filer/js/addons/dropzone.init.js',
             'filer/js/addons/popup_handling.js',
             'filer/js/addons/widget.js',
         )
+        """
 
     def get_url_params(self):
         params = self.url_parameters()
@@ -42,34 +46,59 @@ class FilerGuiFileWidget(AdminFileWidget):
         return url_params
 
     def get_rawid_input(self, name, value, attrs):
-        if 'class' not in attrs:
-            # JavaScript looks for this hook.
-            attrs['class'] = 'vForeignKeyRawIdAdminField'
-        # get only the input html field of ForeignKeyRawIdWidget
-        return super(ForeignKeyRawIdWidget, self).render(name, value, attrs)
+        attrs.setdefault('class', 'vForeignKeyRawIdAdminField')
+        attrs_str = ' '.join('{}="{}"'.format(k, v) for k, v in attrs.items())
+        html = '<input type="text" name="{}" value="{}" {}/>'.format(
+            name,
+            value or '',
+            attrs_str
+        )
+        return mark_safe(html)
 
     def get_context(self, name, value, attrs):
-        obj = self.obj_for_value(value)
+        obj, lookup_url = self.object_and_lookup_url_for_value(value)
         css_id = attrs.get('id', 'id_image_x')
-        if obj:
-            url = obj.logical_folder.get_admin_directory_listing_url_path()
-        else:
-            url = reverse('admin:filer-directory_listing-last')
         url_params = self.get_url_params()
         context = {
             'rawid_input': self.get_rawid_input(name, value, attrs),
-            'lookup_url': '%s%s' % (url, url_params),
+            'lookup_url': '%s%s' % (lookup_url, url_params),
+            'edit_url': '%s%s' % (lookup_url, url_params),
             'object': obj,
             'name': name,
             'css_id': css_id,
         }
         return context
 
-    def render(self, name, value, attrs=None):
+    """
+    def render(self, name, value, attrs=None, renderer=None):
         context = self.get_context(name, value, attrs)
-        html = render_to_string(self.template_name, context)
-        return mark_safe(html)
+        return self._render(self.template_name, context, renderer)
+
+
+    def _render(self, template_name, context, renderer=None):
+        # check if django provides us a renderer (django>=1.11)
+        # if not use render_to_string
+        try:
+            from django.forms.renderers import get_default_renderer
+        except ImportError:
+            return mark_safe(render_to_string(template_name, context))
+        if renderer is None:
+            renderer = get_default_renderer()
+        return mark_safe(renderer.render(template_name, context))
+    """
+
+    def object_and_lookup_url_for_value(self, value):
+        try:
+            key = self.rel.get_related_field().name
+            obj = self.rel.to._default_manager.get(**{key: value})
+        except:
+            obj = None
+        if obj:
+            url = obj.logical_folder.get_admin_directory_listing_url_path()
+        else:
+            url = reverse('admin:filer-directory_listing-last')
+        return obj, url
 
 
 class FilerGuiImageWidget(FilerGuiFileWidget):
-    template_name = 'admin/filer_gui/widgets/admin_image_original.html'
+    template_name = 'admin/filer_gui/widgets/admin_image.html'
