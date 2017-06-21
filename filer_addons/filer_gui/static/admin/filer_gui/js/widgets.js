cd9ca497f5a97d71f6d388c72de3beea5062bd9b
alaric <alaric@wullerot.ch>
2017-06-21 16:13:19 +0200

    new file widget based on ForeignKeyRawIdWidget

--- filer_addons/filer_gui/static/admin/filer_gui/js/widgets.js
+++ filer_addons/filer_gui/static/admin/filer_gui/js/widgets.js
@@ -1,6 +1,34 @@
 var FilerGuiWidgets = (function($){
     'use strict';
 
-    console.log('huiiii')
-    
+    var $body;
+    var $doc = $(document);
+
+    $.fn.filer_gui_file_widget = plugin;
+
+    $doc.on('ready', init);
+
+    function init() {
+        $body = $('body');
+        $('.filer-gui-file-widget').filer_gui_file_widget();
+    };
+
+    function plugin() {
+        return this.each(widget);
+    };
+
+    function widget(i) {
+        var element = this;
+        element.$ = $(this);
+        element.$parent = element.$.parent();
+
+        remove_default_related_links();
+
+        function remove_default_related_links() {
+            $('> .related-widget-wrapper-link', element.$parent).remove();
+        }
+
+        return element;
+    }
+
 })(django.jQuery);
--- filer_addons/filer_gui/widgets.py
+++ filer_addons/filer_gui/widgets.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 import logging
 
+import django
 from django.contrib.admin.widgets import ForeignKeyRawIdWidget
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
@@ -10,14 +11,107 @@ from django.utils.safestring import mark_safe
 
 
 logger = logging.getLogger(__name__)
+django_legacy = django.VERSION < (1, 9)
 
 
 class FilerGuiFileWidget(ForeignKeyRawIdWidget):
     """
-    Adwanced ForeignKeyRawIdWidget with preview and nice select
+    Advanced ForeignKeyRawIdWidget with preview and nice select
     new version
     """
+
     template_name = 'admin/filer_gui/widgets/admin_file.html'
+
+    class Media:
+        if django_legacy:
+            css = {
+                'all': [
+                    'admin/filer_gui/css/filer_gui_legacy.css',
+                ]
+            }
+        else:
+            css = {
+                'all': [
+                    'admin/filer_gui/css/filer_gui.css',
+                ]
+            }
+        js = [
+            'admin/filer_gui/js/widgets.js',
+        ]
+
+    def render(self, name, value, attrs=None, renderer=None):
+        if hasattr(self, '_render'):
+            return super(FilerGuiFileWidget, self).render(
+                name,
+                value,
+                attrs,
+                renderer
+            )
+        else:
+            return self.legacy_render(name, value, attrs)
+
+    def get_context(self, name, value, attrs):
+        context = super(FilerGuiFileWidget, self).get_context(
+            name,
+            value,
+            attrs
+        )
+        file_object = self.file_object_for_value(value)
+        rawid_input = self.get_rawid_input(name, value, attrs)
+        context.update({
+            'file_object': file_object,
+            'rawid_input': rawid_input,
+        })
+        return context
+
+    def file_object_for_value(self, value):
+        try:
+            key = self.rel.get_related_field().name
+            obj = self.rel.to._default_manager.get(**{key: value})
+        except:
+            obj = None
+        return obj
+
+    def get_rawid_input(self, name, value, attrs):
+        attrs.setdefault('class', 'vForeignKeyRawIdAdminField')
+        attrs_str = ' '.join('{}="{}"'.format(k, v) for k, v in attrs.items())
+        html = '<input type="text" name="{}" value="{}" {}/>'.format(
+            name,
+            value or '',
+            attrs_str
+        )
+        return mark_safe(html)
+
+    def legacy_render(self, name, value, attrs=None):
+        rel_to = self.rel.to
+        if attrs is None:
+            attrs = {}
+        if rel_to in self.admin_site._registry:
+            # The related object is registered with the same AdminSite
+            related_url = reverse(
+                'admin:{}_{}_changelist'.format(
+                    rel_to._meta.app_label,
+                    rel_to._meta.model_name,
+                ),
+                current_app=self.admin_site.name,
+            )
+
+            params = self.url_parameters()
+            if params:
+                url = '?' + '&amp;'.join(
+                    '{}={}'.format(k, v) for k, v in params.items()
+                )
+            else:
+                url = ''
+        context = {
+            'file_object': self.file_object_for_value(value),
+            'rawid_input': self.get_rawid_input(name, value, attrs),
+            'related_url': '{}{}'.format(related_url, url),
+            'widget': {
+                'name': name,
+            },
+        }
+        return mark_safe(render_to_string(self.template_name, context))
 
 
 # TODO get rid of filer code and implement rawid field
@@ -110,4 +204,5 @@ class FilerGuiFileWidgetOld(ForeignKeyRawIdWidget):
 
 
 class FilerGuiImageWidget(FilerGuiFileWidget):
-    template_name = 'admin/filer_gui/widgets/admin_image.html'
+    pass
+    # template_name = 'admin/filer_gui/widgets/admin_image.html'
