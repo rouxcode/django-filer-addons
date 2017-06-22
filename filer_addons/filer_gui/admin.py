2999198805e73df9b69c9d50af38da974b4dfe0b
alaric <alaric@wullerot.ch>
2017-06-22 19:12:54 +0200

    py code cleanup

--- filer_addons/filer_gui/admin.py
+++ filer_addons/filer_gui/admin.py
@@ -31,15 +31,11 @@ class FilerGuiAdmin(admin.ModelAdmin):
 
     def file_detail_json_view(self, request):
         form = FilerGuiFileSelectForm(request.POST)
-        print form.is_valid(), form.errors
         if form.is_valid():
             obj = form.cleaned_data.get('filer_file')
-            print obj.file_type
             if obj.file_type == 'Image':
-                print getattr(obj, 'easy_thumbnails_thumbnailer', 'blupper')
                 th = obj.easy_thumbnails_thumbnailer
                 thumb = th.get_thumbnail({'size': THUMBNAIL_SIZE})
-                print thumb, thumb.url
                 thumb_url = thumb.url
             else:
                 thumb_url = obj.icons['48']
@@ -59,9 +55,6 @@ class FilerGuiAdmin(admin.ModelAdmin):
                 'error': 'no valid file id',
             }
         return JsonResponse(data=data)
-
-    def get_thumbnail(self, obj, size=None):
-        size = size or THUMBNAIL_SIZE
 
     def get_model_perms(self, *args, **kwargs):
         """
