0faad2c8b215ce27d987c691039cd022e65011d3
benzkji <bnzk@bnzk.ch>
2017-07-05 13:16:16 +0200

    new settings moder for filer_signals

--- filer_addons/filer_signals/conf.py
+++ filer_addons/filer_signals/conf.py
@@ -5,16 +5,21 @@ from django.conf import settings
 FILER_ADDONS_DUPLICATE_HANDLING = getattr(
     settings, 'FILER_ADDONS_DUPLICATE_HANDLING', {
         'prevent': True,
+        'created_only': True,
         'same_folder_required': True,
         'same_filename_required': True,
-        'rename_files': True,
+        # TODO: as own setting/signal? 'rename_files': True,
     }
 )
 
+FILER_ADDONS_RENAME_FILES = getattr(
+    settings, 'FILER_ADDONS_RENAME_FILES', True
+)
 
 FILER_ADDONS_UNFILED_HANDLING = getattr(
     settings, 'FILER_ADDONS_UNFILED_HANDLING', {
         'move_unfiled': True,
+        'created_only': True,
         'default_folder_name': '0 - Direct Uploads',
     }
 )
