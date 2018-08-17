9f05b70ec5851cbe5519744271b0c7ff96f557aa
benzkji <bnzk@bnzk.ch>
2018-08-17 15:22:31 +0200

    folders count for stats

--- filer_addons/filer_utils/management/commands/subcommands/stats.py
+++ filer_addons/filer_utils/management/commands/subcommands/stats.py
@@ -3,8 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from django.utils.translation import ugettext_lazy as _
 
-from filer.models import File
-from filer.models import Image
+from filer.models import File, Image, Folder
 
 from .base import SubcommandsCommand
 
@@ -20,10 +19,12 @@ class StatsCommand(SubcommandsCommand):
         files_count = File.objects.all().count()
         images_count = Image.objects.all().count()
         files_only = files_count - images_count
+        self.stdout.write("Folders count: %s" % Folder.objects.all().count())
         self.stdout.write("Files count: %s" % files_only)
         self.stdout.write("Images count: % s" % images_count)
         self.stdout.write("Files + images count: %s" % files_count)
         # duplicates
+        self.stdout.write("Computing duplicates...pls wait...")
         duplicates = []
         for file in File.objects.all():
             if not file in duplicates:
