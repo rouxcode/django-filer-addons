96e59d88f731db72f0a782e2f6bff882b034a857
benzkji <bnzk@bnzk.ch>
2019-10-02 16:14:59 +0200

    admin-styles and djangocms-admin-style support

---
+++ filer_addons/tests/settings_admin_styles.py
@@ -0,0 +1,3 @@
+from .settings import *  # noqa
+
+INSTALLED_APPS.insert(0, 'admin_styles')  # noqa
---
+++ filer_addons/tests/settings_cms_admin_style.py
@@ -0,0 +1,3 @@
+from .settings import *  # noqa
+
+INSTALLED_APPS.insert(0, 'djangocms_admin_style')  # noqa
