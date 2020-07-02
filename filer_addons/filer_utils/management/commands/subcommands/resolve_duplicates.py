77abfb8c497c231cec19aeb7d2087fc1a8a57a9b
Ben Stähli <bnzk@bnzk.ch>
2020-07-02 10:24:23 +0200

    experimenting

--- filer_addons/filer_utils/management/commands/subcommands/resolve_duplicates.py
+++ filer_addons/filer_utils/management/commands/subcommands/resolve_duplicates.py
@@ -14,5 +14,34 @@ class ResolveDuplicatesCommand(SubcommandsCommand):
     command_name = 'resolve_duplicates'
 
     def handle(self, *args, **options):
-        # implement! take care!
-        self.stdout.write("Not yet!")
+        resolve_for_class(File)
+        resolve_for_class(Image)
+
+    def resolve_for_class(self, cls):
+        # exit("untested code! exiting!")
+        # file dups
+        temp = cls.objects.values('sha1').annotate(Count('id')).values('sha1').order_by().filter(id__count__gt=1)
+        duplicates = cls.objects.filter(sha1__in=temp).order_by('-created')
+        duplicates_count = duplicates.distinct().count()
+        print("resolving {} duplicates in {}!".format(duplicates_count, cls.__name__))
+        done = {}
+        for file in duplicates:
+            # do we have an original!?
+            if file.sha1 in done:
+                # we have an original. go through all related objects for this file.
+                for related in file._meta._related_objects_cache:
+                    # get all of the same model that use this file
+                    kwargs = {
+                        related.field.name: file.id,
+                    }
+                    affected = related.model.objects.filter(**kwargs)
+                    # update them to use the original
+                    kwargs = {
+                        related.field.name: done[file.sha1],
+                    }
+                    affected.update(**kwargs)
+                # DELETE
+                file.delete()
+            else:
+                # first time, keep this file
+                done[file.sha1] = file.id
