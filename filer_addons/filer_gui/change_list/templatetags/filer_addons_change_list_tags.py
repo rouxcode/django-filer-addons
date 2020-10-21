65bd26375218713cc8d8bd10d727d87d2d88d814
Ben Stähli <bnzk@bnzk.ch>
2020-10-21 13:31:32 +0200

    change list gui: conf/setting for THUMB_SIZE

--- filer_addons/filer_gui/change_list/templatetags/filer_addons_change_list_tags.py
+++ filer_addons/filer_gui/change_list/templatetags/filer_addons_change_list_tags.py
@@ -9,7 +9,7 @@ register = template.Library()
 def filer_addons_change_list_thumb(obj, ):
     if isinstance(obj, Image):
         thumbnailer = get_thumbnailer(obj.file)
-        thumbnail_options = {'size': (170, 120)}
+        thumbnail_options = {'size': conf.CHANGE_LIST_THUMB_SIZE}
         return thumbnailer.get_thumbnail(thumbnail_options).url
     else:
         return obj.icons['48']
