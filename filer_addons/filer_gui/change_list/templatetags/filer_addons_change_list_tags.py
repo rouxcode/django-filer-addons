1bef84f826111e7ee336b901fc45bba4101a0e64
Ben Stähli <bnzk@bnzk.ch>
2021-06-29 22:47:23 +0200

    fix missing files exception

--- filer_addons/filer_gui/change_list/templatetags/filer_addons_change_list_tags.py
+++ filer_addons/filer_gui/change_list/templatetags/filer_addons_change_list_tags.py
@@ -13,6 +13,8 @@ def filer_addons_change_list_thumb(obj, ):
     if isinstance(obj, Image):
         thumbnailer = get_thumbnailer(obj.file)
         thumbnail_options = {'size': conf.CHANGE_LIST_THUMB_SIZE}
-        return thumbnailer.get_thumbnail(thumbnail_options).url
-    else:
-        return obj.icons['48']
+        try:
+            return thumbnailer.get_thumbnail(thumbnail_options).url
+        except (InvalidImageFormatError, FileNotFoundError) as e:
+            pass
+    return obj.icons.get('48', '')
