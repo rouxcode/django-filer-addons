d05e9ecea2f2374940659b70901e3760fcc34770
Ben Stähli <bnzk@bnzk.ch>
2022-02-01 22:19:51 +0100

    invalid image format error

--- filer_addons/filer_gui/change_list/templatetags/filer_addons_change_list_tags.py
+++ filer_addons/filer_gui/change_list/templatetags/filer_addons_change_list_tags.py
@@ -1,6 +1,10 @@
 from django import template
 from easy_thumbnails.exceptions import InvalidImageFormatError
 from easy_thumbnails.files import get_thumbnailer
+# support very big images
+# https://stackoverflow.com/questions/51152059/pillow-in-python-wont-let-me-open-image-exceeds-limit
+import PIL.Image
+PIL.Image.MAX_IMAGE_PIXELS = 933120000
 from filer.models import Image
 
 from .. import conf
